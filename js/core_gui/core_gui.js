77855531af261da775ffb88175be6058fd3b58cf
Tim Villanueva <tim.villanueva@ask.com>
2016-05-13 11:30:19 -0700

    support for nested modals

--- js/core_gui/core_gui.js
+++ js/core_gui/core_gui.js
@@ -1,3 +1,6 @@
+
+"use strict";
+
 //
 // V_GUI 
 //
@@ -6,18 +9,22 @@
 // Requires EJS (ejs_production.ejs), and jQuery
 // provided templates should be in the /templates directory off docroot
 //
+/* globals vgGUI: true */
 
+var vgGUI = null;
 
 $(function() {
-
+	vgGUI = new vgs();
+	console.log ("VGGUI init");
+});
 
 var vgs = function() {
 
 	// some DOM objects that get inserted when needed
-	var mask = "<div id='vgMask' class='vg-mask'></div>";
-	var spin = "<div id='vgSpin' class='ball-loader'> <p> Loading... </p></div>";
-	var modal = "<div class='vg-modal'><div class='vg-modal-head'><div class='vg-close vg-tabable'>&#215</div><div class='vg-title'></div></div><div class='vg-modal-body'></div><div class='vg-btn-bar'></div></div>";
-	var btn = "<div class='vg-btn vg-tabable'></div>";
+	var MASK = "<div id='vgMask' class='vg-mask'></div>";
+	var SPIN = "<div id='vgSpin' class='ball-loader'> <p> Loading... </p></div>";
+	var MODAL = "<div class='vg-modal'><div class='vg-modal-head'><div class='vg-close vg-tabable'>&#215</div><div class='vg-title'></div></div><div class='vg-modal-body'></div><div class='vg-btn-bar'></div></div>";
+	var BTN = "<div class='vg-btn vg-tabable'></div>";
 
 	var that = this;
 	this.maskCount = 0;
@@ -26,12 +33,12 @@ var vgs = function() {
     // keep track of key states
     //
     $(document).keydown(function(e) {
-        if (e.which==16) {
+        if (e.which===16) {
             that.shifted = true;
         }    
     });
     $(document).keyup(function(e) {
-        if (e.which==16) {
+        if (e.which===16) {
             that.shifted = false;
         }    
     });
@@ -43,14 +50,14 @@ var vgs = function() {
 	// using height 100% doesn't work right, so use JS to calc height
 	this.showMask = function() {
 		if (this.maskCount <= 0) {			
-			$("body").prepend(mask);
+			$("body").prepend(MASK);
 			sizeMask();
 			$(window).resize(function() {
 				sizeMask();
 			});
 		}	
 		this.maskCount++;
-	}
+	};
 	//----------------------------------------------------------------
 	// hideMask
 	//
@@ -61,15 +68,15 @@ var vgs = function() {
 			$(".vg-mask").remove();
 			this.maskCount = 0;
 		}	
-	}
+	};
 	//----------------------------------------------------------------
 	// showSpinner
 	//
 	// show the css spinner (bouncing ball)
 	this.showSpinner = function () {
 		that.showMask();
-		$("#vgMask").append(spin);
-	}
+		$("#vgMask").append(SPIN);
+	};
 	//----------------------------------------------------------------
 	// hideSpinner
 	//
@@ -77,7 +84,7 @@ var vgs = function() {
 	this.hideSpinner = function () {
 		that.hideMask();
 		$(".ball-loader").remove();
-	}
+	};
 
 	//----------------------------------------------------------------
 	// showModal
@@ -91,18 +98,18 @@ var vgs = function() {
 
 	var MODAL_ID = 1;
 
-	this.showModal = function(template, options, cb) {
+	this.showModal = function(templateName, options, cb) {
 
-		var html, modalTag, modalElement;
-		var template = new EJS({url: "/templates/" + template + ".ejs"});
+		var html, modalTag, modalElement, modalId;
+		var template = new EJS({url: "/templates/" + templateName + ".ejs"});
 		var data = null;
-		var buttons = ['ok'];
+		var buttons = ["Ok"];
 
 		if (exists(options)) {
-			if ('data' in options) {
+			if ("data" in options) {
 				data = options.data;
 			}
-			if ('buttons' in options) {
+			if ("buttons" in options) {
 				buttons = options.buttons;
 			}
 		}
@@ -110,10 +117,9 @@ var vgs = function() {
 		if (template) {
 			that.showMask();
 
-			modalTag = modal;
+			modalTag = MODAL;
 			modalId = "vg-modal-" + MODAL_ID++;
 			modalElement = $(modalTag).attr("id", modalId);
-			console.log("setting ID to " + modalId);
 
 			$("#vgMask").append(modalElement);
 			modalElement = $("#"+modalId);
@@ -125,8 +131,8 @@ var vgs = function() {
 			// add buttons
 			setupButtons (modalElement.find(".vg-btn-bar"), buttons, cb);
 			// setup actions
-			setBtnClick (modalElement.find('.vg-close'), that.hideModal);
-			modalElement.find('.vg-close').attr("tabindex", 0);
+			setBtnClick (modalElement.find(".vg-close"), that.hideModal);
+			modalElement.find(".vg-close").attr("tabindex", 0);
 
 			centerWindow(modalElement);
 
@@ -135,7 +141,7 @@ var vgs = function() {
 				sizeMask();
 			});
 		}	
-	}
+	};
 
 	//----------------------------------------------------------------
 	// textModal
@@ -145,43 +151,42 @@ var vgs = function() {
 	this.textModal = function(message, btns, cb) {
 		var opts = {title: "Enter Text",
 				data: {msg: message},
-				buttons:['Ok']
-		}
+				buttons:["Ok"]
+		};
 		if (exists(btns)) {
 			opts.buttons = btns;
 		}
 		that.showModal("core_gui/textmodal", opts, function(txt, el) {
-			var retText = $(el.parent()).find('.text-modal-field').val();
+			var retText = $(el.parent()).find(".text-modal-field").val();
 			console.log(retText);
-	  		that.hideModal();
-	  		if (exists(cb)) {
-	  			cb(txt, retText);
-  			}
+			that.hideModal();
+			if (exists(cb)) {
+				cb(txt, retText);
+			}
 		});
-	}
+	};
 	//----------------------------------------------------------------
 	// messageModal
 	//
 	this.messageModal = function(message, btns, cb) {
-		opts = {title: "Feedback! Message",
-				data: {msg: message},
-				buttons:['Ok']
-		}
+		var opts = {title: "Feedback! Message",
+					data: {msg: message},
+					buttons:["Ok"]
+		};
 		if (exists(btns)) {
 			opts.buttons = btns;
 		}
 		that.showModal("core_gui/messagebox", opts, function(txt){
-									  		that.hideModal();
-									  		if (exists(cb)) {
-									  			cb(txt);
-									  		}
+			that.hideModal();
+			if (exists(cb)) {
+				cb(txt);
+			}
 		});
-	}
-
+	};
 
 	this.alertBox = function(message, cb) {
 		that.messageModal(message, null, cb);
-	}
+	};
 
 	//----------------------------------------------------------------
 	// hideModal
@@ -194,7 +199,7 @@ var vgs = function() {
 		if (allWindows.length > 0) {
 			allWindows[allWindows.length-1].remove();
 		}	
-	}
+	};
 
 	//----------------------------------------------------------------
 	// centerWindow
@@ -227,25 +232,25 @@ var vgs = function() {
 		});
 	}
 
+/*
     function stop_tab(e){
-        if(e.target.id === 'vg-btn-ok'){
+        if(e.target.id === "vg-btn-ok"){
             if(e.keyCode === 10){
                 e.preventDefault();
             }
         }
     }
-
+*/
 	//----------------------------------------------------------------
 	// setupButtons
 	//
 	// Setup the buttons based on the strings
-	function setupButtons(el, opts, cb) {
-		var e, i, buttonId;
+	function setupButtons(el, btns, cb) {
+		var e, i;
 
-		for (i=0; i<opts.length; i++) {
-			e = btn;
-			e = $(e).attr("tabindex", i+1);
-			e = $(e).text(opts[i]);
+		for (i=0; i<btns.length; i++) {
+			e = $(BTN).attr("tabindex", i+1);
+			e = $(e).text(btns[i]);
 			$(e).click(function(e) {
 				var txt = $(e.target).text();
 				cb(txt, el);
@@ -257,7 +262,6 @@ var vgs = function() {
 		setupButtonKeyActions ("vg-tabable");
 	}
 
-
 	//----------------------------------------------------------------
 	// setupButtonKeyActions
 	//
@@ -268,27 +272,25 @@ var vgs = function() {
 	function setupButtonKeyActions(classname) {
 
 	    $("." + classname).keydown(function(e) {
-	        if (e.which == 9) {
-	        	e.preventDefault();
-				var numTabable = $('.vg-tabable').length;
-				var nextTab = parseInt($(e.target).attr('tabindex')) + (that.shifted ? -1 : 1);
+	        if (e.which === 9) {
+				e.preventDefault();
+				var numTabable = $(".vg-tabable").length;
+				var nextTab = parseInt($(e.target).attr("tabindex"), 10) + (that.shifted ? -1 : 1);
 				if (nextTab < 0) {
 					nextTab = numTabable-1;
 				}
 				if (nextTab >= numTabable) {
-					nextTab = 0
+					nextTab = 0;
 				}
 				nextTab = Math.min(nextTab, numTabable);
 				nextTab = Math.max(nextTab, 0);
 				$("."+classname)[nextTab].focus();
-	        }
-	        if (e.which == 13) {
-	        	$(e.target).trigger("click");
-	        }
-	    });
+			}
+			if (e.which === 13) {
+				$(e.target).trigger("click");
+			}
+		});
 	}
-}
+};
 
-vgGUI = new(vgs);
 
-});
